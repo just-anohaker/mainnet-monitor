76f0ab83e4c3295b428db750cce259fc959811a1
just-anohaker <just.anohaker@gmail.com>
2019-11-11 17:50:32 +0800

    fixedme

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     app.useWebSocketAdapter(new IoAdapter(app));
     await app.listen(8888);
 }
